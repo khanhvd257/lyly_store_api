7f509cff33471a0ad1969388843a22a95f74a992
Khanh Van Dinh <79238188+khanhvd2002@users.noreply.github.com>
2023-11-01 11:35:48 +0700

    update file và thêm api giỏ hàng
    Signed-off-by: Khanh Van Dinh <79238188+khanhvd2002@users.noreply.github.com>

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\OrderValidation;
+use App\Models\Order;
+use App\Models\OrderDetail;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
+    const PENDING = 'Pending';
+    const CONFIRMED = 'Confirmed';
+    const DONE = 'Done';
 
+    public function orderProduct(OrderValidation $request)
+    {
+        $username = Auth::guard('api')->user()['username'];
+        $product = Product::find($request['product_id']);
+        $checkStock = $product->isAvailableInStock();
+        if (!$checkStock) {
+            return $this->sendError('Kho thiếu hàng');
+        } else {
+            $newOrder = Order::created($username);
+            $data = [
+                'order_id' => $newOrder['id'],
+                'quantity' => $request['quantity'],
+                'product_id' => $request['product_id'],
+                'price' => $request['quantity'] * $product['price'],
+                'delivery_address' => $request['delivery_address'],
+                'note' => $request['node']
+            ];
+            $order = OrderDetail::created($data);
+            return $this->sendResponse($order);
+
+        }
+    }
 }
